ad49b917b4251aa320959254b69c454a8ab882be
Mark Holmes <contact@markholm.es>
2024-12-24 11:16:25 -0600

    whichkey and surround

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map("n", "<Space>wtv", ":vsplit | terminal<CR>", opts)
+map("n", "<Space>wth", ":split | terminal<CR>", opts)
