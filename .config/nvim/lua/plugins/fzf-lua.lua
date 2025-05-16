a397cd5edd231a3774fea600a7af5ad9c367830f
Mark Holmes <mark.holmes@calm.com>
2025-05-16 14:10:36 -0500

    Use fuzzy search

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -3,6 +3,6 @@ return {
   keys = {
     -- Change from root to CWD by default
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    { "<leader>/", LazyVim.pick("grep_project", { root = false }), desc = "Grep (cwd)" },
   },
 }
