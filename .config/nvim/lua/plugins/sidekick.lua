1610983788137b975d3334de44c4f7637a7bd9e2
Mark Holmes <mark.holmes@calm.com>
2025-10-02 14:14:21 -0500

    better maps to default to Claude

---
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/sidekick.nvim",
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        require("sidekick.cli").toggle({ name = "claude", focus = true })
+      end,
+      desc = "Sidekick Toggle",
+    },
+    {
+      "<leader>ac",
+      function()
+        require("sidekick.cli").toggle({ name = "claude", focus = true })
+      end,
+      desc = "Sidekick Claude Toggle *Default*",
+    },
+    {
+      "<leader>ax",
+      function()
+        require("sidekick.cli").toggle({ name = "codex", focus = true })
+      end,
+      desc = "Sidekick Codex Toggle",
+      icon = "Toggle",
+    },
+  },
+}
