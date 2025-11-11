d395c4a83d9c4b85ecbfdd682bcc20458723ae8a
Mark Holmes <contact@markholm.es>
2025-11-10 23:29:27 -0600

    Tab completion added

--- .config/nvim/lua/plugins/sidekick.lua
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -14,6 +14,17 @@ return {
 
     return {
       {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>" -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      {
         "<leader>aa",
         function()
           require("sidekick.cli").toggle({ name = default_sidekick, focus = true })
