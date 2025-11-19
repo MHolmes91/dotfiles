98ce388ff07e8503709f3210f6edcb472fcc5788
Mark Holmes <mark.holmes@calm.com>
2025-11-19 17:56:48 -0600

    Added better Octo mappings

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -2,32 +2,46 @@ return {
   "pwntester/octo.nvim",
   opts = {
     picker = "fzf-lua",
+    mappings = {
+      runs = {
+        expand_step = { lhs = "<localleader>x", desc = "expand workflow step" },
+        open_in_browser = { lhs = "<localleader>b", desc = "open workflow run in browser" },
+        refresh = { lhs = "<localleader>f", desc = "refresh workflow" },
+        rerun = { lhs = "<localleader>ra", desc = "rerun workflow" },
+        rerun_failed = { lhs = "<localleader>rf", desc = "rerun failed workflow" },
+        cancel = { lhs = "<localleader>c", desc = "cancel workflow" },
+        copy_url = { lhs = "<localleader>y", desc = "copy url to system clipboard" },
+      },
+    },
   },
   keys = {
-    { "<leader>Gr", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
-    { "<leader>Gs", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+    -- disable defaults
+    { "<leader>gi", false },
+    { "<leader>gI", false },
+    { "<leader>gp", false },
+    { "<leader>gP", false },
+    { "<leader>gr", false },
+    { "<leader>gS", false },
 
-    { "<leader>Gil", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
-    { "<leader>Gis", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
-    { "<leader>Gii", "", desc = "+issue (Octo)", ft = "octo" },
+    -- Top level
+    { "<leader>Go", "<cmd>Octo<CR>", desc = "Octo Command" },
+    { "<leader>Gr", "<cmd>Octo repo list<CR>", desc = "List Repos" },
+    { "<leader>Gs", "<cmd>Octo search<CR>", desc = "Search" },
 
-    { "<leader>Gpl", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
-    { "<leader>Gps", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
-    { "<leader>Gpf", "<cmd>Octo pr<CR>", desc = "Find branch PR (Octo)" },
-    { "<leader>Gpc", "<cmd>Octo pr create<CR>", desc = "Create branch PR (Octo)" },
-    { "<leader>Gpp", "", desc = "+pr (Octo)", ft = "octo" },
+    -- Issues
+    { "<leader>Gil", "<cmd>Octo issue list<CR>", desc = "List Issues" },
+    { "<leader>Gis", "<cmd>Octo issue search<CR>", desc = "Search Issues" },
 
-    -- These are only available in Octo
-    { "<leader>Gpr", "", desc = "+rebase (Octo)", ft = "octo" },
-    { "<leader>Gps", "", desc = "+squash (Octo)", ft = "octo" },
+    -- PRs
+    { "<leader>Gpl", "<cmd>Octo pr list<CR>", desc = "List PRs" },
+    { "<leader>Gps", "<cmd>Octo pr search<CR>", desc = "Search PRs" },
+    { "<leader>Gpg", "<cmd>Octo pr<CR>", desc = "Goto branch PR" },
+    { "<leader>GpC", "<cmd>Octo pr create<CR>", desc = "Create branch PR" },
 
-    { "<leader>Ga", "", desc = "+assignee (Octo)", ft = "octo" },
-    { "<leader>Gc", "", desc = "+comment/code (Octo)", ft = "octo" },
-    { "<leader>Gl", "", desc = "+label (Octo)", ft = "octo" },
-    { "<leader>Gr", "", desc = "+react (Octo)", ft = "octo" },
-    { "<leader>Gv", "", desc = "+review (Octo)", ft = "octo" },
-    { "<leader>Gg", "", desc = "+goto_issue (Octo)", ft = "octo" },
-    { "@", "@<C-x><C-o>", mode = "i", ft = "octo", silent = true },
-    { "#", "#<C-x><C-o>", mode = "i", ft = "octo", silent = true },
+    -- Local
+    { "<localleader>pD", "<cmd>Octo pr draft<CR>", desc = "Mark PR as draft" },
+    { "<localleader>pR", "<cmd>Octo pr ready<CR>", desc = "Mark PR as ready" },
+    { "<localleader>pC", "<cmd>Octo pr checks<CR>", desc = "View PR Checks" },
+    { "<localleader>pb", "<cmd>Octo pr browser<CR>", desc = "View PR in browser" },
   },
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,11 +1,14 @@
 local wk = require("which-key")
 wk.add({
+  -- Basic Neovim
   { "<leader>wt", group = "Terminal", icon = "" },
-  { "<leader>G", group = "GitHub", icon = "" },
-  { "<leader>Gp", group = "PR", icon = "" },
-  { "<leader>Gi", group = "Issue", icon = "" },
   { "<leader>wtv", ":vsplit | terminal<CR>", desc = "Vertical Terminal", mode = "n" },
   { "<leader>wth", ":split | terminal<CR>", desc = "Horizontal Terminal", mode = "n" },
+
+  -- Octo
+  { "<leader>G", group = "GitHub", icon = "" },
+  { "<leader>Gi", group = "+issue", icon = "" },
+  { "<leader>Gp", group = "+pr", icon = "" },
 })
 
 return {}
